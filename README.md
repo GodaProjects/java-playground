706394a2364fe19c3040f605d028a2492692c6c8
GodaProjects <goda.projects@gmail.com>
2020-08-20 13:32:17 +0530

    Multi-Resolution Image API and other changes

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # ramdom-java-playground
 A host of random java code
 
+### Changes
 1. use of jshell for testing java programs (interactive REPL for java)
 2. Factory methods for collections
 3. More methods added to stream api
 4. private methods in interfaces
+5. Multi-Resolution Image API - Not doing anything here. See this for more details - https://www.geeksforgeeks.org/java-9-features-with-examples/
--- src/org/goda/java9/privatemethodinterface/impl/ASampleImplForInterfaceWithDefaultMethods.java
+++ src/org/goda/java9/privatemethodinterface/impl/ASampleImplForInterfaceWithDefaultMethods.java
@@ -2,7 +2,12 @@ package org.goda.java9.privatemethodinterface.impl;
 
 import org.goda.java9.privatemethodinterface.InterfaceJava9Enhancements;
 
-
+/**
+ * https://www.javatpoint.com/java-9-interface-private-methods#:~:text=Java%209%20Private%20Interface%20Methods&text=Interface%20allows%20us%20to%20declare,throws%20a%20compile%20time%20error.
+ * Interface allows us to declare private methods that help to share common code between non-abstract methods.
+ * Before Java 9, creating private methods inside an interface cause a compile time error. The following example is
+ * compiled using Java 8 compiler and throws a compile time error.
+ */
 public class ASampleImplForInterfaceWithDefaultMethods implements InterfaceJava9Enhancements {
     private String member;
 
